43559f982a0be7cffbcaf12d00c3bd0274580512
bert-alpen <bert.alpen@googlemail.com>
2021-11-11 14:18:52 +0100

    Create solution with initial projects

---
+++ mqtt-graphite-bridge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mqtt_graphite_bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
