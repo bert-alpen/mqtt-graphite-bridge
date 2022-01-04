bc2d8156adb13aa0e468bc176c5eb45546e59745
bert-alpen <bert.alpen@googlemail.com>
2022-01-04 17:32:49 +0100

    connect to MQTT source and log received messages

--- MqttGraphiteBridge/Worker.cs
+++ MqttGraphiteBridge/Worker.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Receiving;
+using MQTTnet.Client.Subscribing;
 
 namespace MqttGraphiteBridge
 {
@@ -18,12 +26,68 @@ namespace MqttGraphiteBridge
             _config = options.Value;
         }
 
+        private IMqttClient CreateSourceClient(Endpoint sourceConfiguration)
+        {
+            var mqttClient = new MqttFactory().CreateMqttClient();
+
+            mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(args =>
+            {
+                _logger.Log(LogLevel.Information, $"Publisher  {sourceConfiguration.Host}:{sourceConfiguration.Port} Connected");
+
+                var sr = mqttClient.SubscribeAsync(sourceConfiguration.Topic);
+                _logger.Log(LogLevel.Information, "Subscribed");
+            });
+
+            mqttClient.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(args =>
+            {
+                _logger.Log(LogLevel.Information, $"Message received for topic {args.ApplicationMessage.Topic}: {System.Text.Encoding.UTF8.GetString(args.ApplicationMessage.Payload)}");
+            });
+
+            mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(args =>
+            {
+                if (args.ClientWasConnected)
+                {
+                    _logger.Log(LogLevel.Information, $"Publisher {sourceConfiguration.Host}:{sourceConfiguration.Port} disconnected. Reason: {args.Reason}");
+                }
+                else
+                {
+                    _logger.Log(LogLevel.Error, $"Connection to publisher {sourceConfiguration.Host}:{sourceConfiguration.Port} failed. Reason: {args.Reason}");
+                }
+            });
+
+            return mqttClient;
+        }
+        private IMqttClientOptions CreateSourceOptions(Endpoint sourceConfiguration, string clientId)
+        {
+            return new MqttClientOptionsBuilder()
+                .WithClientId(clientId)
+                .WithTcpServer(sourceConfiguration.Host, sourceConfiguration.Port)
+                .WithCredentials(sourceConfiguration.UserName, sourceConfiguration.Password)
+                .WithCleanSession()
+                //.WithCommunicationTimeout(new TimeSpan(0, 0, 5))
+                .Build();
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                using (var mqttClient = CreateSourceClient(_config.Source))
+                {
+                    var options = CreateSourceOptions(_config.Source, _config.ClientId);
+                    try
+                    {
+                        var connectionResult = await mqttClient.ConnectAsync(options, stoppingToken);
+                    }
+                    catch (Exception e)
+                    {
+                        _logger.LogError(e.ToString());
+                        return;
+                    }
+
+
+                    await Task.Delay(5000, stoppingToken);
+                }
             }
         }
     }
